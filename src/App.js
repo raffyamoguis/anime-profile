68e873ac7a9ef387ea9c95186c5f348c6c1f38a2
unknown <raffyamoguis@gmail.com>
2022-02-12 11:03:14 +0800

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+
 
 function App() {
   return (
-    <></>
+    <Navbar className="navbarlight" collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container fluid>
+
+        {/* Navbar Logo */}
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt=""
+          />
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+        <Navbar.Collapse id="responsive-navbar-nav">
+
+          <Nav className>
+            <InputGroup>
+              <InputGroup.Text className="search-form-addon" id="search-addon">@</InputGroup.Text>
+              <FormControl
+                className="search-form"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="search-addon"
+              />
+            </InputGroup>
+          </Nav>
+
+          <Nav className="mx-auto" activeKey="/home">
+            <Nav.Item>
+              <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+          </Nav>
+
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+
+        </Navbar.Collapse>
+
+      </Container>
+    </Navbar>
   );
 }
 
